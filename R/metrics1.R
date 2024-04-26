d876ef3aa9de444f0e150ce5c857fc86eba194b2
MTJF5 <mtfriedman@smith.edu>
2024-04-26 08:46:02 -0400

    fixing vol and temp funct

--- R/metrics1.R
+++ R/metrics1.R
@@ -85,74 +85,52 @@ weight <- function(num, starting_unit, final_unit) {
 }
 
 #####################
-
-# Volume conversion function
-volume <-
-  function(num = NULL,
-           starting_unit = "gal",
-           final_unit = "lit") {
-    # Check if num is NULL
-    if (is.null(num)) {
-      stop("Input 'num' cannot be NULL.")
-    }
-
-    # Remove NA values
-    if (anyNA(num)) {
-      warning("Remove NA values")
-      num <- na.omit(num)
-    }
-
-    # Conversion factors
-    conversion_factors <- c("gal" = 3.78541, "lit" = 1)
-
-    # Perform conversion
-    if (starting_unit == "gal" && final_unit == "lit") {
-      result <- num * conversion_factors[2] / conversion_factors[1]
-    } else if (starting_unit == "lit" && final_unit == "gal") {
-      result <- num * conversion_factors[1] / conversion_factors[2]
-    } else {
-      stop("Conversion not supported.")
-    }
-
-    return(result)
+volume <- function(num, starting_unit, final_unit) {
+  if (is.null(num)) {
+    stop("Input cannot be NULL.")
   }
 
-
-# Temperature conversion function
-temp <-
-  function(num = NULL,
-           starting_unit = "c",
-           final_unit = "f") {
-    # Check if num is NA
-    if (is.null(num)) {
-      stop("Input cannot be NULL.")
-    }
-
-    # Remove NA values
-    if (anyNA(num)) {
-      warning("Remove NA values")
-      num <- na.omit(num)
-    }
-
-    # Conversion formula
-    if (starting_unit == "c" && final_unit == "f") {
-      result <- (num * 9 / 5) + 32
-    } else if (starting_unit == "f" && final_unit == "c") {
-      result <- (num - 32) * 5 / 9
-    } else {
-      stop("Conversion not supported.")
-    }
-
-    return(result)
+  # remove NA values
+  if (anyNA(num)) {
+    warning("Remove NA values")
+    num <- num[!is.na(num)]
   }
 
-# Test volume conversion
-volume(num = 10,
-       starting_unit = "gal",
-       final_unit = "lit")  # Expected output: 37.8541
-volume(
-  num = c(10, 20, 30),
-  starting_unit = "gal",
-  final_unit = "lit"
-)  # Expected output: c(37.8541, 75.7082, 113.5624)
+  if (starting_unit == "gal" && final_unit == "lit") {
+    converted <- num * 3.78541
+    return(converted)
+  }
+
+  if (starting_unit == "lit" && final_unit == "gal") {
+    converted <- num / 3.78541
+    return(converted)
+  }
+}
+
+# Test case
+result <- volume(5, "gal", "lit")
+print(result)  # Expected output: 18.92705
+
+#####################
+temp <- function(num, starting_unit, final_unit) {
+  if (is.null(num)) {
+    stop("Input cannot be NULL.")
+  }
+  # remove NA values
+  if (anyNA(num)) {
+    warning("Remove NA values")
+    num <- num[!is.na(num)]
+  }
+
+  if (starting_unit == "c" && final_unit == "f") {
+    result <- (num * (9/5)) + 32
+  } else if (starting_unit == "f" && final_unit == "c") {
+    result <- (num - 32) * (5/9)
+  } else {
+    stop("Conversion not supported.")
+  }
+
+  return(result)
+}
+#####################
 
